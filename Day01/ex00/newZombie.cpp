c491eed3a1ddeb1b3c41b4ac4cd9d71459c58a71
Mohamed KARIM <karimkar999@gmail.com>
2022-11-05 11:02:11 +0100

    some changes

--- Day01/ex00/newZombie.cpp
+++ Day01/ex00/newZombie.cpp
@@ -6,17 +6,7 @@
 /*   By: mkarim <mkarim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 15:37:07 by mkarim            #+#    #+#             */
-/*   Updated: 2022/10/31 09:35:44 by mkarim           ###   ########.fr       */
+/*   Updated: 2022/11/03 15:00:32 by mkarim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Zombie.hpp"
-
-Zombie* Zombie::newZombie( std::string name )
-{
-    Zombie* newZombie = new Zombie(name);
-    if (!newZombie)
-        return newZombie;
-    newZombie->announce();
-    return newZombie;
-}
