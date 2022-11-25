0c38b3924177bd6bad18d213a53a1ca61bf434e7
Mohamed KARIM <karimkar999@gmail.com>
2022-11-25 19:36:20 +0100

    Start working on day 07 of cpp piscine this day made to learn Template concept in c++ i'm now finished to first exercise

---
+++ Day07/ex00/whatever.hpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkarim <mkarim@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/25 18:16:33 by mkarim            #+#    #+#             */
+/*   Updated: 2022/11/25 19:34:26 by mkarim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+#include <iostream>
+
+template <typename T>
+void    swap(T &a, T &b)
+{
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
+template <typename T>
+T min(T a, T b)
+{
+    return (a < b) ? a : b;
+}
+
+template <typename T>
+T max(T a, T b)
+{
+    return (a > b) ? a : b;
+}
+
+#endif
